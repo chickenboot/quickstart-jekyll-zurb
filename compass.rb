0d05f42486656b45ffbde88047bccb8aab208775
mrpies <mrpies@tritonetechnology.com>
2013-03-15 14:15:10 +0000

    Layout haml watcher to avoid recompiling when default layout changes

--- compass.rb
+++ compass.rb
@@ -17,3 +17,15 @@ sass_dir = "_sass"
 output_style = :compressed
 preferred_syntax = :sass
 relative_assets = true
+
+# -----------------------------------------------
+# haml watcher
+# -----------------------------------------------
+watch "#{File.dirname(__FILE__)}/**/haml/*.haml" do |project_dir, relative_path|
+  if File.exists?(File.join(project_dir, relative_path))    
+    target = File.join(project_dir, relative_path.gsub('haml/','').gsub('.haml', '.html'))
+    source = File.join(project_dir, relative_path)
+    puts "Generating #{target} from #{source}"
+    `bundle exec haml #{File.join(project_dir, relative_path)} #{target}`
+  end
+end
